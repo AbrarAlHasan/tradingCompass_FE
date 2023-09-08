102a93cad691a9315b06c4c4295beda80385409c
Abrar Al Hasan <abraralhasan111@gmail.com>
2023-09-08 21:53:31 +0530

    React Router Install and Implement Basic Structure

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,10 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import Dashboard from "./Screens/Dashboard";
+import Router from "./Router/Router";
 
 function App() {
-  return <Dashboard />;
+  return <Router />;
 }
 
 export default App;
